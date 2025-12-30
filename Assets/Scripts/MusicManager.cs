da10a4f4f9c2d105a99dfc4e43d42c1f7f370a66
Turambar3791 <andrzejszary06@gmail.com>
2025-12-30 16:56:32 +0100

    Added cursor

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    [Header("Music")]
     public AudioSource musicSource;
     public AudioClip backgroundMusic;
 
-    void Start()
+    void Awake()
     {
         musicSource.clip = backgroundMusic;
         musicSource.loop = true;
