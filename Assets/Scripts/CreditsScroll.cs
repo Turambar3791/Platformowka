e51ac6109ccd0139c5eb838b9d8bf3097bc398e0
Turambar3791 <andrzejszary06@gmail.com>
2025-12-31 14:31:52 +0100

    Added text of credits

--- Assets/Scripts/CreditsScroll.cs
+++ Assets/Scripts/CreditsScroll.cs
@@ -22,8 +22,9 @@ public class CreditsScroll : MonoBehaviour
     {
         rectTransform.anchoredPosition += Vector2.up * speed * Time.deltaTime;
 
-        if (rectTransform.anchoredPosition.y > endY)
+        if ((rectTransform.anchoredPosition.y > endY) || Input.GetKeyDown(KeyCode.Escape))
         {
+            Debug.Log("QUIT");
             Application.Quit();
         }
     }
